3aa0a8c28704f38f4a8ee715d61e5da5b6574388
SenorPez <senorpez@senorpez.com>
2017-06-03 23:24:49 -0500

    Updates to gradle wrapper and dependencies.

--- src/main/java/com/senorpez/projectcars/SwaggerConfig.java
+++ src/main/java/com/senorpez/projectcars/SwaggerConfig.java
@@ -12,6 +12,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 
@@ -23,25 +24,27 @@ class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfo(
                         "Project CARS Data API",
-                        "Project CARS static data API.",
-                        "0.1.0-rc1",
+                        "Project CARS static data API",
+                        "Version 1",
                         "http://projectcars.senorpez.com/tos.html",
-                        new Contact("SenorPez", "http://senorpez.com/", "senorpez@senorpez.com"),
+                        new Contact("SenorPez", "http://senorpez.com", "senorpez@senorpez.com"),
                         "MIT License",
-                        "https://github.com/SenorPez/project-cars-data-api/blob/master/LICENSE"
+                        "https://github.com/SenorPez/project-cars-data-api/blob/master/LICENSE",
+                        new ArrayList<>()
                 ))
-                .host("projectcars.senorpez.com")
-                .produces(new HashSet<>(Collections.singletonList("application/json")))
+                .produces(new HashSet<>(Collections.singletonList("application/vnd.senorpez.pcars.v1+json; charset=UTF-8")))
                 .protocols(new HashSet<>(Collections.singletonList("http")))
                 .tags(
                         new Tag("cars", "Project CARS Cars Data"),
-                        new Tag("events", "Project CARS Events Data"),
+                        new Tag("classes", "Project CARS Car Class Data"),
+                        new Tag("events", "Project CARS Single Player Events Data"),
+                        new Tag("races", "Project CARS Single Player Races Data"),
+                        new Tag("rounds", "Project CARS Single Player Rounds Data"),
                         new Tag("tracks", "Project CARS Tracks Data")
                 )
-                .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(Predicates.not(PathSelectors.regex("/error|/v1$|^/$")))
+                .paths(Predicates.not(PathSelectors.regex("/error|^/$")))
                 .build();
     }
 }
